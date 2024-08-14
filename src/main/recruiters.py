2d30a044c937e5698488e206e2de6bd6b519478b
vladiuse <vladiuse@gmail.com>
2024-08-14 17:46:08 +0300

    remove recruiters list from module

--- src/main/recruiters.py
+++ src/main/recruiters.py
@@ -1,25 +1,5 @@
 import csv
 
-recruiters = [
-    'Aaron Barton',
-    'Alex Adeli',
-    'Barry Heath',
-    'Brian Weller',
-    'Christal Hurrington',
-    'Jamar Pharr',
-    'James Ireland',
-    'Jessica Burg',
-    'Karen Freedman',
-    'Kathryn Lamastra',
-    'Mark Herschberg',
-    'Matt Hertsenberg',
-    'Megan Marshall',
-    'Nick McGill',
-    'Peter Nooteboom',
-    'Shardai Johnson',
-    'Sydney Buckner',
-]
-
 
 def get_recruiters_data(file_path: str) -> list:
     recruiters_data = list()
