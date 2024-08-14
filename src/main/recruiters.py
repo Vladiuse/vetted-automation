1a3cf4e381bc2b9ee807945bf6d8abcb09eeda8d
vladiuse <vladiuse@gmail.com>
2024-08-14 13:31:51 +0300

    add recruiters module

---
+++ src/main/recruiters.py
@@ -0,0 +1,22 @@
+recruiters = [
+        'Aaron Barton',
+        'Alex Adeli',
+        'Barry Heath',
+        'Brian Weller',
+        'Christal Hurrington',
+        'Jamar Pharr',
+        'James Ireland',
+        'Jessica Burg',
+        'Karen Freedman',
+        'Kathryn Lamastra',
+        'Mark Herschberg',
+        'Matt Hertsenberg',
+        'Megan Marshall',
+        'Nick McGill',
+        'Peter Nooteboom',
+        'Shardai Johnson',
+        'Sydney Buckner',
+    ]
+
+class Recruiters:
+    pass
