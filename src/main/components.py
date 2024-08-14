efa0aa5e932252efc63b0b1880f2f5af64200f38
vladiuse <vladiuse@gmail.com>
2024-08-14 18:37:39 +0300

    add type annotation to components.py

--- src/main/components.py
+++ src/main/components.py
@@ -2,6 +2,7 @@ import random as r
 from time import sleep
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 from .exceptions import NoSuchRecruiterInForm
 from .recruiters import recruiters
@@ -18,10 +19,10 @@ class ClinicalConversationForm:
         self.message_box = self.elem.find_element(By.TAG_NAME, 'textarea')
         self.send_msg_btn = form.find_element(By.CSS_SELECTOR, 'button:has(img[src="/images/icons/send.svg"])')
 
-    def insert_message(self, message: str):
+    def insert_message(self, message: str) -> None:
         self.message_box.send_keys(message)
 
-    def submit(self):
+    def submit(self) -> None:
         self.send_msg_btn.click()
 
 
@@ -34,7 +35,7 @@ class Clinical:
     def __init__(self, elem):
         self.elem = elem
 
-    def open_conversation(self):
+    def open_conversation(self) -> None:
         self.elem.click()
 
 
@@ -66,16 +67,16 @@ class TransferForm:
     """
     LOCATOR = (By.CSS_SELECTOR, 'form:not([class])')
 
-    def __init__(self, elem, driver):
+    def __init__(self, elem:WebElement, driver):
         self.driver = driver
         self.elem = elem
         self.input = self.elem.find_element(By.CSS_SELECTOR, 'input.rw-dropdownlist-search')
         self.submit_btn = self.elem.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
 
-    def get_options(self):
+    def get_options(self) -> list[WebElement]:
         return self.elem.find_elements(By.CSS_SELECTOR, 'div[role="option"]')
 
-    def get_option_by_rec_name(self, recruiter_name: str):  # TODO maybe need to delete, old func
+    def get_option_by_rec_name(self, recruiter_name: str) -> WebElement:
         options = self.get_options()
         option_to_return = None
         for option in options:
@@ -88,7 +89,7 @@ class TransferForm:
             raise NoSuchRecruiterInForm
         return option_to_return
 
-    def get_random_option(self):
+    def get_random_option(self) -> WebElement:
         available_recruiters_names = recruiters.get_active_rec_names()
         available_recruiters_names = list(map(lambda name: name.lower(), available_recruiters_names))
         options = self.get_options()
@@ -101,17 +102,17 @@ class TransferForm:
         choice = r.choice(options_to_chose)
         return choice
 
-    def submit(self):
+    def submit(self) -> None:
         self.submit_btn.click()
 
-    def chose_recruiter_by_name(self, name:str):
+    def chose_recruiter_by_name(self, name: str) -> None:
         option = self.get_option_by_rec_name(name)
         self.driver.execute_script("arguments[0].scrollIntoView();", option)
         sleep(1)
         self.driver.execute_script("arguments[0].click();", option)
         self.driver.execute_script("arguments[0].style.backgroundColor = 'red';", option)
 
-    def chose_random_recruiter(self):
+    def chose_random_recruiter(self) -> None:
         option = self.get_random_option()
         self.driver.execute_script("arguments[0].scrollIntoView();", option)
         sleep(1)
@@ -139,7 +140,7 @@ class ConvFilterForm:
     """
     LOCATOR = (By.CSS_SELECTOR, 'form[class*=modal-header-height]')
 
-    def __init__(self, elem):
+    def __init__(self, elem:WebElement):
         self.elem = elem
         self.check_boxes = [
             self.elem.find_element(By.ID, 'checkbox-Engaged'),
@@ -147,12 +148,12 @@ class ConvFilterForm:
         ]
         self.submit_btn = self.elem.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
 
-    def _off_checkboxes(self):
+    def _off_checkboxes(self) -> None:
         for checkbox in self.check_boxes:
             sleep(1.5)
             checkbox.click()
 
-    def clear_n_submit(self):
+    def clear_n_submit(self) -> None:
         sleep(1)
         self._off_checkboxes()
         sleep(1)
