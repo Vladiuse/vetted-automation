2bd73789eeaac09b5e6409b4df2b66c4507ec9ff
vladiuse <vladiuse@gmail.com>
2024-08-15 16:55:02 +0300

    fix path of data files

--- src/main/config.py
+++ src/main/config.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ENV_PATH = BASE_DIR / '../.env'
-STATES_PATH = BASE_DIR / '../states.json'
-RECRUITERS_PATH = BASE_DIR / '../recruiters.csv'
+ENV_PATH = BASE_DIR.parent / '.env'
+STATES_PATH = BASE_DIR.parent / 'states.json'
+RECRUITERS_PATH = BASE_DIR.parent / 'recruiters.csv'
